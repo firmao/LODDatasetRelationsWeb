d1788dfd265d567a918de4950719556feef14dad
Andre Valdestilhas <firmao@gmail.com>
2019-07-19 21:28:14 +0200

    Add files via upload

--- src/web/servlet/matching/MatchingServlet.java
+++ src/web/servlet/matching/MatchingServlet.java
@@ -58,9 +58,13 @@ public class MatchingServlet extends HttpServlet {
 						+ "  width: 100%;\n" + "}\n" + "\n" + "td, th {\n" + "  border: 1px solid #dddddd;\n"
 						+ "  text-align: left;\n" + "  padding: 8px;\n" + "}\n" + "\n" + "tr:nth-child(even) {\n"
 						+ "  background-color: #dddddd;\n" + "}\n" + "</style>\n" + "</head>\n" + "<body>");
-				bRet.append("<h1>Results</h1>");
+				bRet.append("<h1>Input: </h1><textArea cols=\"100\" rows=\"3\">" + dataset + "</textArea>");
+				bRet.append("<h1>Output:</h1>");
 				int count = 0;
-				for (String r : ret) {
+				for (String element : ret) {
+					String r = element.toLowerCase();
+					r = r.replaceAll("_sparql", "/sparql");
+					r = r.replaceAll("out_tests2/", "");
 					if (r.contains("{")) {
 						bRet.append("<h3>Exact Match</h3>"
 								+ "<br>Json file containing: Property->DatasetsMatch");
@@ -90,15 +94,20 @@ public class MatchingServlet extends HttpServlet {
 						String sR = r.replaceAll("ANDRE_", "");
 						String s[] = sR.split("\t");
 						if (s.length > 3) {
-							bRet.append("<tr>\n" + "    <td>" + s[0] + "</td>\n" + "    <td>" + s[1] + "</td>\n"
-											+ "    <td>" + s[2] + "</td>\n" + "    <td>" + s[3] + "</td>\n"
+							String pS = "<a href='"+ s[0] +"'>" + s[0] + "</a>";
+							String pT = "<a href='"+ s[1] +"'>" + s[1] + "</a>";
+							String dS = getLink(s[2]);
+							String dT = getLink(s[3]);
+							bRet.append("<tr>\n" + "    <td>" + pS + "</td>\n" + "    <td>" + pT + "</td>\n"
+											+ "    <td>" + dS + "</td>\n" + "    <td>" + dT + "</td>\n"
 											+ "  </tr>");
 						} else {
 							if (count == 0) {
-								// response.getOutputStream().println("<h2>Dataset similar:</h2>");
-								bRet.append("<table><tr><th>" + s[0] + "</th><th>"	+ s[1] + "</th><th>" + s[2] + "</th></tr>");
+								//bRet.append("<table><tr><th>" + s[0] + "</th><th>"	+ s[1] + "</th><th>" + s[2] + "</th></tr>");
+								bRet.append("<table><tr><th>Dataset</th><th>#Exact Match</th><th>#Similarity > 0.9</th></tr>");
 							} else {
-								bRet.append("<tr>\n" + "    <td>" + s[0] + "</td>\n" + "    <td>"
+								String ds = getLink(s[0]);
+								bRet.append("<tr>\n" + "    <td>" + ds + "</td>\n" + "    <td>"
 										+ s[1] + "</td>\n" + "    <td>" + s[2] + "</td>\n" + "  </tr>");
 							}
 							count++;
@@ -111,6 +120,14 @@ public class MatchingServlet extends HttpServlet {
 			response.getOutputStream().println(bRet.toString());
 			// response.getWriter().append("Served at:
 			// ").append(request.getParameter("query"));
+		}
+	}
+
+	private String getLink(String ds) {
+		if(ds.contains("sparql")) {
+			return "<a href='http://"+ ds +"'>http://" + ds + "</a>";
+		} else {
+			return "<a href='http://139.18.13.76:8082/dirHDTLaundromat/decompressed/"+ ds.substring(0, 2) + "/" + ds.replaceAll(".hdt", "/") +"'>" + ds + "</a>";
 		}
 	}
 
